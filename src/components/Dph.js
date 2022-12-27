7da673409dce616f3e67b6fa38acabdb0d2fb7e8
AkshitTheComputerScientist <akshit360@live.com>
2022-12-27 15:06:51 +0000

    Added form submission logic and css changes

--- src/components/Dph.js
+++ src/components/Dph.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import moon from '../assets/moon.png';
 import {useEffect, useState} from 'react';
+import './Dph.css';
   
 const Dph = () => {
 
   const [priceOfNFT, setPrice] = useState(null)
   const [inputs, setInputs] = useState({});
+  const [formSubmitted, setFormSubmit] =useState(null);
+  const [formError, setFormError] = useState(null)
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -14,6 +17,13 @@ const Dph = () => {
   }
 
   const handleSubmit = (event) => {
+    if(inputs.salary === undefined || !inputs.nft){
+      setFormError(true)
+    }
+    else{
+      setFormError(null)
+      setFormSubmit(true)
+    }
     event.preventDefault();
     console.log(inputs.salary / priceOfNFT)
   }
@@ -36,58 +46,86 @@ const Dph = () => {
   });
 }, [inputs]);
 
-
   return (
+    <div className="container dph-container" id="dphContainer">  
+      <div className="columns">
+          <div className="column is-8">
+              <h1 className="dph-title"> Diamond Paper Hands </h1>
+              <h3 className="dph-subtitle"> Find out how long you need to work until you can afford that NFT you always wanted instead of paying off that mortgage </h3>
+          </div>
 
-<div class="container dph-container">  
-  <div class="columns">
-      <div class="column is-8">
-          <h1 class="dph-title"> Diamond Paper Hands </h1>
-          <h3 class="dph-subtitle"> Find out how long you need to work until you can afford that NFT you always wanted instead of paying off that mortgage </h3>
+          <div className="column is-4">
+              <img className='moon-pic' src={moon} />
+          </div>
       </div>
 
-      <div class="column is-4">
-          <img className='moon-pic' src={moon} />
+        <div> 
       </div>
-  </div>
 
-    <div> 
-  </div>
+    <div className="columns is-multiline dph-body-container">
+      <div className="column is-7 form-container">
 
-<form onSubmit={handleSubmit}>
-      <label>Enter your annual salary:
-      <input 
-        type="number" 
-        name="salary" 
-        value={inputs.salary || ""} 
-        onChange={handleChange}
-        placeholder="50000"
-      />
-      </label>
-      <label> NFT
-      <select 
-      // type="number" 
-          name="nft" 
-          // value={inputs.nft || ""} 
-          onChange={handleChange}>
-        <option disabled selected value> -- select an option -- </option>
-        <option class='option-dph' value="mutant-ape-yacht-club">MAYC</option>
-        <option class='option-dph' value="boredapeyachtclub">BAYC</option>
-        <option class='option-dph' value="sappy-seals">Sappy Seals</option>
-        <option class='option-dph' value="doodles-official">Audi</option>
-      </select>
-        {/* <input 
-          type="number" 
-          name="nft" 
-          value={inputs.nft || ""} 
-          onChange={handleChange}
-        /> */}
-        </label>
-        <input type="submit" />
-    </form>
+      <form onSubmit={handleSubmit} className="form is-horizontal">
+          <div className='columns is-vcentered'>
+            <div className='column'>
+              <label className='form-label'> Enter your annual Salary ($): </label>
+            </div>
+            <div className='column has-text-right is-4'>
+              <input
+                className='input input-salary is-medium' 
+                type="number" 
+                name="salary" 
+                value={inputs.salary || ""} 
+                onChange={handleChange}
+                placeholder="50000"
+              />
+            </div>
+          </div>
 
+          <div className='columns is-vcentered'>
+            <div className='column'>
+              <label className='form-label'> NFT: </label>
+            </div>
+            <div className='column has-text-right'>
+              <div class="select">
+                <select 
+                  required
+                  className='select is-medium' 
+                  name="nft" 
+                  onChange={handleChange}
+                >
+                  <option disabled selected id="selectOption"> -- select  option -- </option>
+                  <option className='option-dph' value="mutant-ape-yacht-club">MAYC</option>
+                  <option className='option-dph' value="boredapeyachtclub">BAYC</option>
+                  <option className='option-dph' value="sappy-seals">Sappy Seals</option>
+                  <option className='option-dph' value="doodles-official">Audi</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <button className="button" id="submitButtonDph" type="submit">
+              Submit
+          </button>
+        </form>
+      </div>
+      
+      {formSubmitted &&
+        <div className='column has-text-centered'>
+          output gif
+        </div>
+      }
 
-</div>
+      </div>
+
+      {formError &&
+      <article className="message is-danger">
+        <div class="message-body">
+          Please submit your salary and select an NFT
+        </div>
+      </article>
+      }
+      
+    </div>
   );
 };
   
